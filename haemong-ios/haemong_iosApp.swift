070ac29bacc92ecc4034518d18cda37179699a6b
izak.kang <izak.kang@factblock.com>
2025-07-25 15:41:47 +0900

    Initial Commit

---
+++ haemong-ios/haemong_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  haemong_iosApp.swift
+//  haemong-ios
+//
+//  Created by Kang Seongchan on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct haemong_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ haemong-iosTests/haemong_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  haemong_iosTests.swift
+//  haemong-iosTests
+//
+//  Created by Kang Seongchan on 7/25/25.
+//
+
+import Testing
+@testable import haemong_ios
+
+struct haemong_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
